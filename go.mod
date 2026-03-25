ee14dc803a13891daaca9d7596a4467ff63bbc18
Yusuke Nomura <yunomu@gmail.com>
2026-03-25 22:14:22 +0900

    Add test

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/aws/smithy-go v1.24.0
 	github.com/bluesky-social/indigo v0.0.0-20251223190123-598fbf0e146e
 	github.com/google/subcommands v1.2.0
+	github.com/stretchr/testify v1.10.0
 )
 
 require (
@@ -36,6 +37,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sso v1.30.9 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.35.13 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.41.6 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/earthboundkid/versioninfo/v2 v2.24.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
@@ -68,6 +70,7 @@ require (
 	github.com/multiformats/go-multihash v0.2.3 // indirect
 	github.com/multiformats/go-varint v0.0.7 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/polydawn/refmt v0.89.1-0.20221221234430-40501e09de1f // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/whyrusleeping/cbor-gen v0.2.1-0.20241030202151-b7a6831be65e // indirect
@@ -81,5 +84,6 @@ require (
 	golang.org/x/crypto v0.21.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.2.1 // indirect
 )
---
+++ lib/consumer/dailyjsons3_test.go
@@ -0,0 +1,119 @@
+package consumer
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	smithy "github.com/aws/smithy-go"
+	"github.com/bluesky-social/indigo/api/bsky"
+	"github.com/bluesky-social/indigo/lex/util"
+	"github.com/stretchr/testify/assert"
+)
+
+// MockS3Client is a mock implementation of the S3Client interface.
+type MockS3Client struct {
+	PutObjectFunc func(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+	GetObjectFunc func(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
+}
+
+func (m *MockS3Client) PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error) {
+	if m.PutObjectFunc != nil {
+		return m.PutObjectFunc(ctx, params, optFns...)
+	}
+	return &s3.PutObjectOutput{}, nil
+}
+
+func (m *MockS3Client) GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
+	if m.GetObjectFunc != nil {
+		return m.GetObjectFunc(ctx, params, optFns...)
+	}
+	return nil, &smithy.OperationError{
+		Err: &types.NoSuchKey{},
+	}
+}
+
+func TestDailyJSONRecordS3FirstCallOnce(t *testing.T) {
+	ctx := context.Background()
+
+	var firstCallCount atomic.Int32
+
+	mockS3 := &MockS3Client{
+		PutObjectFunc: func(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error) {
+			return &s3.PutObjectOutput{}, nil
+		},
+		GetObjectFunc: func(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
+			if *params.Key == "base/2023/01/index" || *params.Key == "base/2023/02/index" {
+				return nil, &smithy.OperationError{
+					Err: &types.NoSuchKey{},
+				}
+			}
+			return &s3.GetObjectOutput{
+				Body: io.NopCloser(bytes.NewReader([]byte{})),
+			}, nil
+		},
+	}
+
+	c := NewDailyJSONRecordS3(
+		mockS3,
+		"test-bucket",
+		"base",
+		time.UTC,
+		SetDailyJSONRecordS3FirstValueFunc(func(v *TerminalValue) {
+			firstCallCount.Add(1)
+		}),
+	)
+
+	post1 := &bsky.FeedDefs_FeedViewPost{
+		Post: &bsky.FeedDefs_PostView{
+			Cid: "cid1",
+			Record: &util.LexiconTypeDecoder{
+				Val: &bsky.FeedPost{
+					CreatedAt: time.Date(2023, 1, 1, 10, 0, 0, 0, time.UTC).Format(time.RFC3339Nano),
+					Text:      "test1",
+				},
+			},
+		},
+	}
+	post2 := &bsky.FeedDefs_FeedViewPost{
+		Post: &bsky.FeedDefs_PostView{
+			Cid: "cid2",
+			Record: &util.LexiconTypeDecoder{
+				Val: &bsky.FeedPost{
+					CreatedAt: time.Date(2023, 1, 1, 11, 0, 0, 0, time.UTC).Format(time.RFC3339Nano),
+					Text:      "test2",
+				},
+			},
+		},
+	}
+	post3 := &bsky.FeedDefs_FeedViewPost{
+		Post: &bsky.FeedDefs_PostView{
+			Cid: "cid3",
+			Record: &util.LexiconTypeDecoder{
+				Val: &bsky.FeedPost{
+					CreatedAt: time.Date(2023, 2, 1, 12, 0, 0, 0, time.UTC).Format(time.RFC3339Nano), // Change date to trigger ensureStream.
+					Text:      "test3",
+				},
+			},
+		},
+	}
+
+	err := c.Consume(ctx, post1)
+	assert.NoError(t, err)
+
+	err = c.Consume(ctx, post2)
+	assert.NoError(t, err)
+
+	err = c.Consume(ctx, post3) // ensureStream is called, starting a new stream.
+	assert.NoError(t, err)
+
+	err = c.Close(ctx)
+	assert.NoError(t, err)
+
+	assert.Equal(t, int32(1), firstCallCount.Load(), "first should be called exactly once")
+}
